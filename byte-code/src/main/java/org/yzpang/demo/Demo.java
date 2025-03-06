09f5b4676432d135d4ab055a5df25324813077a0
yzpang <18856833949@163.com>
2025-03-06 15:45:25 +0800

    构建入门示例

---
+++ byte-code/src/main/java/org/yzpang/demo/Demo.java
@@ -0,0 +1,12 @@
+package org.yzpang.demo;
+
+/**
+ * Author: yzpang
+ * Desc:
+ * Date: 2025/3/6 下午2:26
+ **/
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
